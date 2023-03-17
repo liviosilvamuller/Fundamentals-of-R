4898824b5facac22a840fea335f4470e8e7dd733
liviosilvamuller <livio.silva@graduateinstitute.ch>
2023-03-17 10:51:52 +0100

    case study files

--- Fundamentals_workshop_2023/Case Study/Cleaning theses/Cleaning_Theses.R
+++ Fundamentals_workshop_2023/Case Study/Cleaning theses/Cleaning_Theses.R
@@ -4,14 +4,14 @@
 # Date: January 2023
 
 library(tidyverse)
-library(cld3)
-library(readxl)
+library(cld3) #detect languages of text
 
 setwd("~/Documents/GitHub/Fundamentals_of_R_IHEID2022/Fundamentals_workshop_2023/Case Study/Cleaning theses")
 dirty_phd_theses <- readRDS("dirty_phd_theses.rds")
 skimr::skim(dirty_phd_theses)
 
 #Data cleaning dirty_phd_theses
+
 
 dirty_phd_theses <- dirty_phd_theses %>%
   mutate(year=replace_na(year,"2022"), # all missing years are actually 2022
@@ -29,9 +29,8 @@ dirty_phd_theses <- dirty_phd_theses %>%
            program == "PhD in Development Economics (2014-)" ~  "IE",
            program == "PhD in International Law (2012-)" ~  "IL",
            program == "No specialisation (1928-2001)" ~  "Other"),
-           complete_series  =ifelse(year>2013, TRUE, FALSE))%>%#establishing the grounds of comparison)%>%
+           complete_series  =ifelse(year>2012, TRUE, FALSE))%>%#establishing the grounds of comparison)%>%
   select(-year2, -program) #getting rid of useless variables
-
 
 #preparing variables for analysis
 
@@ -39,14 +38,11 @@ dirty_phd_theses$language<- detect_language(dirty_phd_theses$abstract) #detect l
 dirty_phd_theses$language <- ifelse(dirty_phd_theses$language=="ig", detect_language(dirty_phd_theses$title), dirty_phd_theses$language) # package codes NAs as igbo...
 dirty_phd_theses$language <- gsub("sr", "en", dirty_phd_theses$language) #all "sr" are english (manual check)
 
-dirty_phd_theses <- dirty_phd_theses %>%
-  mutate(language=replace_na(language,"en")) # all NAs are actually english
-
 sus_en <- ("sustain| enviro| agric |rural development |clean technologies|
                    climate change| carbon emissions| greenhouse gases| natural disasters|
                    conservation| biodiversity| corporate responsibility| energy|
                    natural resources| water| human security| equity| social inequality|
-                   social justice|  poverty| peace| urban| land| health ")
+                   social justice|  poverty| peace| urban| land| health")
 
 sus_fr <- ("durabl| enviro| agric | rural |
               développement | technologies propres| changement climatique| émissions de carbone|
@@ -67,7 +63,7 @@ dirty_phd_theses <- dirty_phd_theses %>%
                     stringr::str_count(dirty_phd_theses$abstract,sus_fr),
                     stringr::str_count(dirty_phd_theses$abstract,sus_en)),
     gen =ifelse(dirty_phd_theses$language=="fr",
-                  stringr::str_count(dirty_phd_theses$abstract,gen_en),
-                  stringr::str_count(dirty_phd_theses$abstract,gen_fr)))
+                  stringr::str_count(dirty_phd_theses$abstract,gen_fr),
+                  stringr::str_count(dirty_phd_theses$abstract,gen_en)))
 
 saveRDS(dirty_phd_theses, "cleaned_phd_theses.rds")
